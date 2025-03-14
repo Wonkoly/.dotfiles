1f23b5365c2b975fe8935f7c563b07fa057d179f
Wonkoly <gaelmaga96@gmail.com>
2025-03-13 19:54:46 -0600

    Creacion de funcion para designarle un grupo a las aplicaciones, en hooks

--- .config/qtile/hooks.py
+++ .config/qtile/hooks.py
@@ -10,3 +10,22 @@ def set_transparency(window):
         window.opacity = 1.3  # Firefox
     elif window.name and "Code-OSS" in window.name:
         window.opacity = 1.3 
+        
+# Asignacion de grupos por aplicaciones
+@hook.subscribe.client_managed
+def move_window_to_group(window):
+    """Mueve ciertas aplicaciones a grupos específicos al abrirse."""
+    app_group_map = {
+        "firefox": "2",  # Firefox en el grupo 'web'
+        "kitty": "1",    # Kitty en el grupo 'term'
+        "discord": "4",  # Discord en el grupo 'chat'
+        "spotify": "5", # Spotify en el grupo 'music'
+        "code": "3", # Spotify en el grupo 'music'
+    }
+
+    wm_class = window.get_wm_class()  # Obtiene la clase de la ventana
+    if wm_class:
+        for app, group in app_group_map.items():
+            if app in wm_class:
+                window.togroup(group)
+                break
