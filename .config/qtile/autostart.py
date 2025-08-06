7bb79002616cf8258687f3a74b1ddef5589ba662
Wonkoly <111265351+Wonkoly@users.noreply.github.com>
2025-08-06 14:38:08 -0600

    refactor: replace autostart shell with python

--- .config/qtile/autostart.py
+++ .config/qtile/autostart.py
@@ -1,6 +1,26 @@
 import os
 import subprocess
 
-def autostart():
-    home = os.path.expanduser("~")
-    subprocess.Popen([home + "/.config/qtile/autostart.sh"])
+
+COMMANDS = [
+    ["picom", "--config", os.path.expanduser("~/.config/picom/picom.conf")],
+    ["brightnessctl", "set", "50%"],
+    ["feh", "--bg-scale", os.path.expanduser("~/Images/soul-eater1.jpg")],
+    ["nm-applet"],
+    ["blueman-applet"],
+    ["volumeicon"],
+    ["dunst"],
+    ["cbatticon"],
+    ["xset", "r", "rate", "300", "50"],
+    ["kitty"],
+    ["firefox"],
+    ["/home/cokie/Applications/Todoist.AppImage"],
+    ["spotify-launcher"],
+]
+
+
+def run():
+    """Launch autostart applications."""
+    for cmd in COMMANDS:
+        subprocess.Popen(cmd)
+
