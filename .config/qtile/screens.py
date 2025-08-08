0596da5a32665ab88476c18dd229f72e813a2819
Fernando Gael Hernandez Magaña <111265351+Wonkoly@users.noreply.github.com>
2025-08-08 13:23:05 -0600

    refactor: unify bar visibility handling (#7)

--- .config/qtile/screens.py
+++ .config/qtile/screens.py
@@ -2,10 +2,14 @@ from libqtile.config import Screen
 from widgets import widgets
 from libqtile import bar
 from theme import colors, bar_size
+from bar_toggle import DEFAULT_BAR_VISIBILITY, bar_states
 
-show_bar = True  # Esta variable puede ser modificada dinámicamente
+# Create a screen with a bottom bar and initialise its visibility according to
+# the global state stored in ``bar_states``. The same approach can be extended to
+# multiple screens if needed.
+screens = [Screen(bottom=bar.Bar(widgets, bar_size, background=colors["background"]))]
 
-if show_bar:
-    screens = [Screen(bottom=bar.Bar(widgets, bar_size, background=colors["background"]))]
-else:
-    screens = [Screen()]
+for i, screen in enumerate(screens):
+    bar_states.setdefault(i, DEFAULT_BAR_VISIBILITY)
+    if not bar_states[i]:
+        screen.bottom.show(False)
