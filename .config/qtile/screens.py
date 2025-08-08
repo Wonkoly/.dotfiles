98733c820e7b1e1b3b7b7c5ff26bcebd3cb40f99
Fernando Gael Hernandez Magaña <111265351+Wonkoly@users.noreply.github.com>
2025-08-08 12:23:00 -0600

    use theme font and bar size (#6)

--- .config/qtile/screens.py
+++ .config/qtile/screens.py
@@ -1,11 +1,11 @@
 from libqtile.config import Screen
 from widgets import widgets
 from libqtile import bar
-from theme import colors
+from theme import colors, bar_size
 
 show_bar = True  # Esta variable puede ser modificada dinámicamente
 
 if show_bar:
-    screens = [Screen(bottom=bar.Bar(widgets, 26, background=colors["background"]))]
+    screens = [Screen(bottom=bar.Bar(widgets, bar_size, background=colors["background"]))]
 else:
     screens = [Screen()]
